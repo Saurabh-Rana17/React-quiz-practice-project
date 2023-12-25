558c11172d4eccf4b1c2876ded9fb4f457044b48
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-12-25 22:42:37 +0530

    moved state down

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestionTimer from "./QuestionTimer";
 import Answers from "./Answers";
+import QUESTION from "../question";
 
-export default function Question({
-  questionText,
-  answers,
-  onSelectAnswer,
-  onTimeout,
-  selectedAnswer,
-  answerState,
-}) {
+export default function Question({ onTimeout, index, onSelectAnswer }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: answer === QUESTION[index].answers[0],
+      });
+    }, 1000);
+    setTimeout(() => {
+      onSelectAnswer(answer);
+    }, 2000);
+  }
+  let answerState = "";
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    answerState = "answered";
+  }
   return (
     <>
       <div id="question">
         <QuestionTimer timeout={10000} onTimeout={onTimeout} />
-        <h2>{questionText}</h2>
+        <h2>{QUESTION[index].text}</h2>
         <Answers
-          answers={answers}
-          selectedAnswer={selectedAnswer}
+          answers={QUESTION[index].answers}
+          selectedAnswer={answer.selectedAnswer}
           answerState={answerState}
-          onSelect={onSelectAnswer}
+          onSelect={handleSelectAnswer}
         />
       </div>
     </>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,38 +2,20 @@ import React, { useCallback } from "react";
 import { useState } from "react";
 import QUESTION from "../question";
 import quizCompleteIcon from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer";
-import Answers from "./Answers";
 import Question from "./Question";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
-  // userAnswers.length;
+  const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTION.length;
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
   ) {
-    setAnswerState("answered");
-
     setUserAnswers((prev) => {
       const arr = [...prev, selectedAnswer];
       return arr;
     });
-
-    setTimeout(() => {
-      if (selectedAnswer === QUESTION[activeQuestionIndex].answers[0]) {
-        setAnswerState("correct");
-      } else {
-        setAnswerState("wrong");
-      }
-      setTimeout(() => {
-        setAnswerState("");
-      }, 2000);
-    }, 1000);
   },
   []);
 
@@ -59,12 +41,9 @@ export default function Quiz() {
     <div id="quiz">
       <Question
         key={activeQuestionIndex}
-        answers={QUESTION[activeQuestionIndex].answers}
-        onSelectAnswer={handleSelectAnswer}
-        questionText={QUESTION[activeQuestionIndex].text}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
-        answerState={answerState}
+        index={activeQuestionIndex}
         onTimeout={handleTimeout}
+        onSelectAnswer={handleSelectAnswer}
       />
     </div>
   );
